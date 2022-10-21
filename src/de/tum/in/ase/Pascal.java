38a91dc6f5704db1fc3cdfb4a712b413e446bb65
Tunar Mahmudov <tunar.mahmudov@tum.de>
2022-10-21 11:59:38 +0200

    Exercise-Template pushed by Artemis

---
+++ src/de/tum/in/ase/Pascal.java
@@ -0,0 +1,28 @@
+package de.tum.in.ase;
+
+
+import java.util.ArrayList;
+
+import static de.tum.in.ase.InputReader.readInt;
+/**
+ * Represents the Pascal Triangle
+ */
+public class Pascal {
+    /**
+     * This method gets the input n and returns the corresponding 2D ArrayList that will hold the triangle
+     * @param n     number of lines
+     * @return      2D ArrayList of Integer holding the triangle
+     */
+    public static ArrayList<ArrayList<Integer>> pascalTriangle(int n) {
+        // TODO: build and return 2D triangle list
+        return null;
+    }
+
+    /**
+     * Main method
+     * @param args  arguments
+     */
+    public static void main(String[] args) {
+        // TODO: read user input n and print the triangle of height n
+    }
+}
