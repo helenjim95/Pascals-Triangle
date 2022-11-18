63a4e79a7279fac25f86092bab408298dbf4e904
ge32goy <hanyin.chen@tum.de>
2022-11-18 11:29:23 +0100

    output

--- src/de/tum/in/ase/Pascal.java
+++ src/de/tum/in/ase/Pascal.java
@@ -2,6 +2,8 @@ package de.tum.in.ase;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static de.tum.in.ase.InputReader.readInt;
 /**
@@ -13,9 +15,23 @@ public class Pascal {
      * @param n     number of lines
      * @return      2D ArrayList of Integer holding the triangle
      */
-    public static ArrayList<ArrayList<Integer>> pascalTriangle(int n) {
+
+
+    public static ArrayList<ArrayList<Integer>> pascalTriangle(int numberOfRows) {
         // TODO: build and return 2D triangle list
-        return null;
+        ArrayList<ArrayList<Integer>> total_rows = new ArrayList<>(); //result
+        ArrayList<Integer> row = new ArrayList<>();
+        for(int i = 0; i < numberOfRows; i++) {
+            row.add(0, 1); //add element 1 at index 0.
+            //now iterate the row, keep adding elements in pair, starting from j=1,
+            // and set the sum at index j
+            for(int j = 1; j < row.size() - 1; j++) {
+                row.set(j, row.get(j) + row.get(j + 1));
+            }
+            //create a copy of row and store it in all_rows
+            total_rows.add(new ArrayList<>(row));
+        }
+        return total_rows;
     }
 
     /**
@@ -24,5 +40,12 @@ public class Pascal {
      */
     public static void main(String[] args) {
         // TODO: read user input n and print the triangle of height n
+        int numberOfLines = InputReader.readInt("Enter the number of lines:");
+        ArrayList<ArrayList<Integer>> triangle = pascalTriangle(numberOfLines);
+//        TODO: Print: All individual columns are separated by tabs \t.
+        for (int i = 0; i <triangle.size() ; i++) {
+            List<Integer> current = triangle.get(i);
+            System.out.println(Arrays.toString(current.toArray()));
+        }
     }
 }
